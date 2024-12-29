41c2062473bb166ee52d356512a37a4d855a0c59
Nicoco <91542808+OdeeyuhLog@users.noreply.github.com>
2024-12-29 16:23:10 +0800

    Fix: Minor code fix

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.urlencoded({ extended: true }));
-app.use("view engine", "ejs");
+app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", messageRoutes);
